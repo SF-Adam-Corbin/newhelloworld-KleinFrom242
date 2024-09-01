f6ef076595eda4cb3395f560843988c239a80cdc
Klein <136825187+KleinFrom242@users.noreply.github.com>
2024-09-01 15:08:55 -0400

    added print statement for function

--- mine.py
+++ mine.py
@@ -5,3 +5,4 @@ def hello_world():
   # Create a variable that has the value of "Hello world!" so that the function will return the appropriate value.
   string = "Hello world!"
   return string
+print(hello_world)
